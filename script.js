aa4f23c933a50043eb98696a7f79f11f8169a7ca
Rashel124 <webcoderitrashel124@gmail.com>
2025-03-09 05:09:00 -0800

    Js function

---
+++ script.js
@@ -0,0 +1,85 @@
+
+// =======================subtruction==================================================
+
+function  sub(number1, number2)
+{
+    let digit1 = number1;
+    let digit2 = number2;
+    
+    let result =digit1-digit2;
+
+    console.log("The sub is="+result);
+}
+
+// ===========================================summision===================================
+function sum(x, y)
+
+{
+let digit1 = x;
+let digit2 = y;
+
+let result = digit1+digit2;
+
+console.log("The sum is="+result);
+
+}
+
+// ===================================Multiplication===================================
+
+function mul(x1, y1)
+
+{
+let digit1 = x1;
+let digit2 = y1;
+
+let result = digit1*digit2;
+
+console.log("The mul is="+result);
+
+}
+
+// ===================================Division============================================
+
+
+function div(x2, y2)
+
+{
+let digit1 = x2;
+let digit2 = y2;
+
+let result = digit1/digit2;
+
+console.log("The div is="+result);
+
+}
+
+
+
+
+// =====================================================================================
+
+sub(50, 30);
+
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+
+sub(90, 30);
+sum(100, 50);
+mul(20, 10);
+div(30, 10);
+
+
+// console.log("Hello World");
+
+// let number1 = 10;
+// let number2 = 20;
+
+// let result = number1+number2;
+// console.log("The sum is="+result);
+
+
+// sub();
+// sub();
